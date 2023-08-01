d3cc2329669732da7f6631630c27a67ca8640acb
bgtrbgtr <jhmwmw@gmail.com>
2023-08-01 13:47:53 +0300

    fixed link in api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,7 +6,7 @@ export const fetchRouteGeometry = async (
 ): Promise<Array<LatLngExpression> | unknown> => {
   try {
     const response = await axios.get(
-      `http://router.project-osrm.org/route/v1/driving/${coordinates}?geometries=geojson&overview=full&alternatives=false`
+      `https://router.project-osrm.org/route/v1/driving/${coordinates}?geometries=geojson&overview=full&alternatives=false`
     );
     return response.data.routes[0].geometry.coordinates;
   } catch (error: unknown) {
